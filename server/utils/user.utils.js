bcb0126957712b12b8bcc39e428ee6a1c03ceca8
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2024-12-11 19:55:14 +0530

    skip not done

--- server/utils/user.utils.js
+++ server/utils/user.utils.js
@@ -1,29 +1,62 @@
 // Helper to remove a user from the waiting queue
 function removeUserFromQueue(userId, waitingUsers) {
-    const index = waitingUsers.findIndex(user => user.id === userId);
-    if (index !== -1) {
-        waitingUsers.splice(index, 1); // Remove the user from the queue
-    }
+  const index = waitingUsers.findIndex((user) => user.id === userId);
+  if (index !== -1) {
+    waitingUsers.splice(index, 1); // Remove the user from the queue
+  }
 }
 
 function disconnectUserFromRoom(userId, rooms) {
-    for (const roomId in rooms) {
-        const roomUsers = rooms[roomId];
+  for (const roomId in rooms) {
+    const roomUsers = rooms[roomId];
 
-        const userIndex = roomUsers.findIndex(user => user.id === userId);
+    const userIndex = roomUsers.findIndex((user) => user.id === userId);
 
-        if (userIndex > -1) {
-            const partner = roomUsers.find((user, index) => index !== userIndex);
+    if (userIndex > -1) {
+      const partner = roomUsers.find((user, index) => index !== userIndex);
 
-            if (partner) {
-                partner.socket.emit("partner-disconnected", userId);
-            }
+      if (partner) {
+        partner.socket.emit("partner-disconnected", userId);
+      }
 
-            delete rooms[roomId];
-            break;
-        }
+      delete rooms[roomId];
+      break;
     }
+  }
 }
 
+function handleSkip(id, roomId, rooms, waitingUsers, users) {
+  const room = rooms[roomId];
+  if (!room) {
+    console.log(`Room with ID ${roomId} does not exist.`);
+    return;
+  }
 
-module.exports = { removeUserFromQueue, disconnectUserFromRoom };
+  // Find the skipping user and their partner
+  const userIndex = room.findIndex((user) => user.id === id);
+
+  if (userIndex > -1) {
+    const partner = room.find((user, index) => index !== userIndex);
+
+    if (partner) {
+      // Notify the partner about the skip
+      partner.socket.emit("partner-disconnected");
+
+      partner.socket.leave(roomId);
+      users[id].prev = partner.id;
+      // Add the partner back to the waiting pool
+      waitingUsers.push({ socket: partner.socket, id: partner.id });
+    }
+
+    // Add the skipping user back to the waiting pool
+    const currentUser = room[userIndex];
+    currentUser.socket.leave(roomId);
+    waitingUsers.push({ socket: currentUser.socket, id: currentUser.id });
+
+    // Remove the room
+    delete rooms[roomId];
+    console.log(`Room ${roomId} has been deleted.`);
+  }
+}
+
+module.exports = { removeUserFromQueue, disconnectUserFromRoom, handleSkip };
